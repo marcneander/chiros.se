abe505acd9a20d055f84d6ed5fed01896e7876d1
Marc Neander <marcneander@gmail.com>
2016-05-05 13:51:29 +0200

    Chiros.se

---
+++ README.md
@@ -0,0 +1,6 @@
+# chiros.se
+Small presentation website of chiros.se
+
+## Get up and running
+1. npm install
+2. gulp
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var config = require('./gulp.config')();
+var wiredep = require('wiredep');
+var $ = require('gulp-load-plugins')({lazy: true});
+
+gulp.task('webserver', function() {
+    return gulp
+        .src([config.temp, './'], {read: false})
+        .pipe($.webserver({
+            open: true,
+            fallback: 'index.html'
+        }));
+});
+
+gulp.task('less', function() {
+    return gulp
+        .src(config.styles.appLess)
+        .pipe($.less({
+            paths: config.bowerComponents
+        }))
+        .pipe(gulp.dest(config.temp + '/css/'));
+});
+
+gulp.task('html', function() {
+    return gulp
+        .src(config.indexHtml)
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('cname', function() {
+    return gulp
+        .src(config.cname)
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.styles.allLess, ['less']);
+    gulp.watch(config.js, ['js']);
+    gulp.watch(config.indexHtml, ['html']);
+});
+
+gulp.task('default', ['cname', 'html', 'less', 'webserver', 'watch']);
