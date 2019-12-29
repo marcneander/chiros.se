fc4ed679814938a3a5811c9b3869c7eaf1add288
Marc Neander <you@example.com>
2019-12-29 16:16:48 +0100

    Generate favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,17 +15,19 @@ const TemplateWrapper = ({ children }) => {
                 <title>{title}</title>
                 <meta name="description" content={description} />
 
-                <link rel="apple-touch-icon" sizes="180x180" href={`${withPrefix('/')}img/apple-touch-icon.png`} />
-                <link rel="icon" type="image/png" href={`${withPrefix('/')}img/favicon-32x32.png`} sizes="32x32" />
-                <link rel="icon" type="image/png" href={`${withPrefix('/')}img/favicon-16x16.png`} sizes="16x16" />
+                <link rel="apple-touch-icon" sizes="180x180" href={`${withPrefix('/')}apple-touch-icon.png`} />
+                <link rel="icon" type="image/png" sizes="32x32" href={`${withPrefix('/')}favicon-32x32.png`} />
+                <link rel="icon" type="image/png" sizes="16x16" href={`${withPrefix('/')}favicon-16x16.png`} />
+                <link rel="manifest" href={`${withPrefix('/')}site.webmanifest`} />
+                <link rel="mask-icon" href={`${withPrefix('/')}safari-pinned-tab.svg`} color="#ffcc00" />
 
-                <link rel="mask-icon" href={`${withPrefix('/')}img/safari-pinned-tab.svg`} color="#ff4400" />
-                <meta name="theme-color" content="#fff" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="theme-color" content="#ffffff" />
 
                 <meta property="og:type" content="business.business" />
                 <meta property="og:title" content={title} />
-                <meta property="og:url" content="/" />
-                <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+                <meta property="og:url" content={withPrefix('/')} />
+                <meta property="og:image" content={`${withPrefix('/')}/img/logo-full.jpg`} />
             </Helmet>
             <div>{children}</div>
             <Footer />
